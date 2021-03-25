cf282290dec8e52a05572939a5233a3f1fdd74d0
Mamoru Miura <14muller@gmail.com>
2021-03-25 23:12:39 +0900

    Finish toy app

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210324215842) do
+ActiveRecord::Schema.define(version: 20210325135651) do
 
   create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci" do |t|
-    t.text "context"
+    t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
