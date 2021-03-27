5206e2d80a93631b05b58a38a5709b8e89e49eca
muller-ruby <75562393+muller-ruby@users.noreply.github.com>
2021-03-27 20:11:12 +0900

    Initialize repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
   #resources :microposts
   #resources :users
+  get 'static_pages/home'
+  get 'static_pages/help'
   root 'application#hello'
   #root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
