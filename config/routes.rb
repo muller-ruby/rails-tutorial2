85eeb96da046d6362de1f383750c48da4d8dc6b8
muller-ruby <75562393+muller-ruby@users.noreply.github.com>
2021-04-01 08:42:08 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  root 'application#hello'
-  #get 'static_pages/home', to: 'static_pages#home'
+  root 'static_pages#home'
 
-  #get 'static_pages/help', to: 'static_pages#help'
+  get 'static_pages/home', to: 'static_pages#home'
+
+  get 'static_pages/help', to: 'static_pages#help'
+
+  get  'static_pages/about', to: 'static_pages#about'
+
+  get 'static_pages/contact'
+
+
 
 end
