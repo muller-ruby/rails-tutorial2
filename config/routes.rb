4d4dcf5928b4cc81238c045214232749422c1c0b
muller-ruby <75562393+muller-ruby@users.noreply.github.com>
2021-04-01 07:19:18 +0900

    Initialize repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'static_pages/home', to: 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'static_pages/help', to: 'static_pages#help'
 
 end
