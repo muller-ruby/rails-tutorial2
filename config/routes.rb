a0a3c806cac2abb426a9e4d74b607a97ef642cf9
muller-ruby <75562393+muller-ruby@users.noreply.github.com>
2021-04-01 07:24:42 +0900

    Add hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home', to: 'static_pages#home'
+  root 'application#hello'
+  #get 'static_pages/home', to: 'static_pages#home'
 
-  get 'static_pages/help', to: 'static_pages#help'
+  #get 'static_pages/help', to: 'static_pages#help'
 
 end
